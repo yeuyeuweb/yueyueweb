31f5cf17dcae29fadecf9e64c208e9fd2b871275
yueyue <yuxinyue_web@163.com>
2021-02-27 19:23:18 +0800

    京东首页

---
+++ jd/js/index.js
@@ -0,0 +1,14 @@
+var renderEnded = new Array(2);
+$('.header > .w').load('./head/header.html', function (res) {
+  var self = this;
+})
+
+$('.fs > .w').load('./fs/fs.html', function (res) {
+  var self = this;
+});
+
+
+$('.shortcut > .w').load('./shortcut/index.html', function () {
+})
+
+$('.seckill > .w').load('./seckill/index.html')
